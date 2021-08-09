b6894bb40f8e39727be9bec7e8dfd29f18e9ba72
edo777 <edgar.khachatryan95@mail.ru>
2021-08-09 19:35:25 +0400

    event bus creation: Receiving Events

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
 
 const app = express();
 
@@ -9,11 +10,13 @@ app.use(express.json())
 
 const commentsByPostId = {};
 
+// Get comments of post
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+// Create comment
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex'); 
     const { content } = req.body;
 
@@ -22,8 +25,24 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+     // Emit to events
+     await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data : {
+            id: commentId,
+            content,
+            postId: req.params.id,
+        }
+    });
+
     res.status(201).send(comments)
 });
+
+app.post("/events", (req, res) => {
+    console.log('Received Event', req.body);
+
+    res.send({});
+})
 
 app.listen(4001, () => {
     console.log("Listening on 4001");
